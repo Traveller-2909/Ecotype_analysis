e75166d4d6ce20ac61d7cdc936527772d6353049
TimoR <rittweg@igb-berlin.de>
2023-08-21 10:35:11 +0100

    Revised cluster plots for supplement, corrected error in sample size description for phenotype transect plots

--- funcs.R
+++ funcs.R
@@ -3,11 +3,11 @@ interpol <- function(series, length, element){
   ids <- NULL
   values <- NULL
   
-  IDOto <- unique(series$ID)
+  IDOto <- unique(series$fish_ID)
   
   for (i in 1:length(IDOto)) {
     id <- rep(IDOto[i], length.out=length)
-    value <- approx(series[which(series$ID==IDOto[i]), element],
+    value <- approx(series[which(series$fish_ID==IDOto[i]), element],
                     method = "linear", n=length, rule = 2)[2]
     ids <- c(ids, id)
     values <- unlist(c(values, value))
